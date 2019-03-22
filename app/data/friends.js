a98b31573bc65e7166ac7a6b71aa1637320768ed
Derek Ericson <derek.ericson@gmail.com>
2019-03-21 23:49:43 -0700

    Update friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friendsArray = [
+    {
+        name: "Pedro Martinez!",
+        photo: "https://ak1.ostkcdn.com/images/products/L10862311.jpg",
+        scores: [1, 2, 2, 3, 1, 5, 4, 4, 4, 3]
+    },
+    {
+        name: "R Kelly!",
+        photo: "https://pixel.nymag.com/imgs/daily/vulture/2019/01/15/15-r-kelly-criminal-record.w700.h700.jpg",
+        scores: [3, 5, 2, 1, 5, 1, 3, 2, 2, 2]
+    },
+    {
+        name: "Tiger Woods!",
+        photo: "https://ewxh42ah7n11ss2i825ghdq1-wpengine.netdna-ssl.com/wp-content/uploads/sites/7/2019/03/TW_Players_R_4_FI_1.jpg",
+        scores: [4, 4, 5, 3, 2, 1, 3, 2, 2, 2]
+    },
+    {
+        name: "Elon Musk!",
+        photo: "https://static.theceomagazine.net/wp-content/uploads/2018/10/15093202/elon-musk-1100x733.jpg",
+        scores: [2, 3, 1, 4, 4, 1, 5, 5, 3, 5]
+    },
+    {
+        name: "PewDiePie!",
+        photo: "https://cdn.i-scmp.com/sites/default/files/styles/1200x800/public/d8/images/methode/2019/03/20/18ddf00a-4a02-11e9-8e02-95b31fc3f54a_image_hires_025413.JPG?itok=aw_inQuj",
+        scores: [1, 2, 2, 5, 5, 3, 3, 3, 3, 3]
+    }
+    ];
+    
+    module.exports = friendsArray;
